dc01506e583d2ded92cc7d0d65af6f9a0e8292ba
ElisaIanda <elisaianda01@gmail.com>
2023-07-13 17:09:11 +0200

    funzione che aggiorna il countdown e msg finale

--- js/main.js
+++ js/main.js
@@ -1,64 +1,71 @@
-const countdownTimer = document.getElementById("countdown-timer");
-const ora = document.getElementById("ora");
-const minuto = document.getElementById("minuto");
-const secondo = document.getElementById("secondo");
-const finalMessage = document.querySelector("final-message");
-let now;
-
 // setInterval dà il via ad una routine dopo un certo numero di secondi o minuti
 // setTimeout fa partire uno script dopo un tempo prestabilito
 // clearInterval serve a stoppare una routine dinamica, in genere lanciata col metodo setInterval
 
 
-// Rappresenta la data e l'ora correnti
-now = new Date ();
-console.log(now);
-
-// const time = now.getTime();
-// console.log(time);
-
-const date1 = now;
-// const date2 = new Date ('July 13, 2023 15:41:00');
-
-const date2 = new Date ('July 14, 2023 09:30:00');
-
-// Ottengo valore in millisecondo
-let time = date2.getTime() - date1.getTime();
-// countdownTimer.innerHTML = (time);
-console.log(time);
-
-// Convertire in ore min e sec
-
-// divido il tempo ottenuto per 1000 t sec per 60 t min e 60 t ore
-console.log(time / 1000 / 60 / 60);
-
-// Mi ritorna il numero specifico di ore minuti e secondi
-
-const hours = Math.floor(time / 1000 / 60 / 60);
-console.log(hours);
-
-const minutes = Math.floor(time / 1000 / 60 ) % 60;
-console.log(minutes);
-
-const seconds = Math.floor(time / 1000 ) % 60;
-console.log(seconds);
-
-
-// Impostare un messaggio alla fine del countdown
-// setTimeout = time;
-// if (setTimeout === 0){
-//     finalMessage.innerHTML = ("Il tempo di attesa è finito");
-// }
+let now;
 
 // Dichiaro la funzione
-
-
+// Uso setInterval che crea un ciclo ripetuto dopo 1 secondo
+// Metto dentro la funzione setInterval il nome della mia funzione dove creo il calcolo del tempo rimanente
+setInterval(continuedHours, 100)
 continuedHours();
 
-function continuedHours(){
+// Ho spostato il calcolo del tempo dentro la funzione.
+// Questa funzione parte dalla data attuale a domani, 
+// calcolo il tempo rimanente prima in millisecondi
+// e poi divido il tempo in 
+// / 1000 millisecondi per avere i secondi
+// /1000 /60 in millesecondi e secondi per avere il minuto
+// /1000 /60 / 60 in millesecondi e secondi e ora per avere l'ora
 
+function continuedHours() {
 
-    countdownTimer.innerHTML = ("ore: " + hours + " "+ "minuti: " + minutes + " "+ "secondi: " + seconds);
+    // Dichiaro le mie variabili
+    const countdownTimer = document.getElementById("countdown-timer");
+    const finalMessage = document.getElementById("final-message");
+
+    // Rappresenta la data e l'ora correnti
+    now = new Date();
+    console.log(now);
+
+    // const time = now.getTime();
+    // console.log(time);
+
+    const date1 = now;
+    const date2 = new Date('July 13, 2023 17:07:00');
+    // const date2 = new Date('July 14, 2023 09:30:00');
+
+    // Ottengo valore in millisecondo
+    let time = date2.getTime() - date1.getTime();
+    // countdownTimer.innerHTML = (time);
+    console.log(time);
+
+    // Convertire in ore min e sec
+
+    // divido il tempo ottenuto per 1000 t sec per 60 t min e 60 t ore
+    console.log(time / 1000 / 60 / 60);
+
+    // Mi ritorna il numero specifico di ore minuti e secondi
+
+    const hours = Math.floor(time / 1000 / 60 / 60);
+    console.log(hours);
+
+    const minutes = Math.floor(time / 1000 / 60) % 60;
+    console.log(minutes);
+
+    const seconds = Math.floor(time / 1000) % 60;
+    console.log(seconds);
+
+    // Stampo in html il tempo
+    countdownTimer.innerHTML = ("ore: " + hours + " " + "minuti: " + minutes + " " + "secondi: " + seconds);
+
+    // Impostare un messaggio alla fine del countdown
+    if (time <= 0) {
+        finalMessage.innerHTML = ("Il tempo di attesa è finito");
+    }else if (time = 0){
+        // Non riesco a fermare il tempo quando arriva alla fine
+    }
 
 }
 
